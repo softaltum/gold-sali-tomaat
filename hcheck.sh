37cc0bc4e91a733b50cb790fa08ca8e1eda9493c
SoftAltum <softaltum@gmail.com>
2019-11-10 14:15:44 -0600

    Update 1 to existing file

--- hcheck.sh
+++ hcheck.sh
@@ -4,19 +4,26 @@ clear
 
 str="	Hi There"
 echo $str
-str1=$(cat /proc/meminfo| grep -i memtotal|awk '{print $2}')
-x=$((str1/1048756)) 
-echo $x
-str2=`cat /proc/meminfo | grep -i memavailable|awk '{print $2}'`
-y=$((str2/1048756))
-printf "\n"
-printf "\n"
-echo $str1
-echo $str2
-printf " Total Memory is => %.2f" $x 
-printf "\n"
-printf " Available Memory is => %.2f\n" $y
 
-z=$((x/y*100))
-printf " Percentage is => %.2f\n" $z
+str1=$(cat /proc/meminfo| grep -i memtotal|awk '{print $2}')
+# printf " The Total Memory is "
+x=$(awk "BEGIN {print $str1/1048756}")
+echo " The Total Memory is => " $x
+# printf " %.1f"$x
+
+str2=`cat /proc/meminfo | grep -i memavailable|awk '{print $2}'`
+# printf " The Available Memory is "
+y=$(awk "BEGIN {print $str2/1048756}")
+echo " The Available Memory is => " $y
+
+printf "\n"
+RESULT=$(awk "BEGIN {printf \"%.2f\",$y/$x*100}")
+echo " Percentage Memory availability is => " $RESULT
+# z=$((y/x*100))
+#echo $(( $y / $x ))
+
+#echo $(( 100 * 1 / 3 ))
+
+#echo $z
+#printf " Percentage is => %.2f\n" $z
 printf "\n"
